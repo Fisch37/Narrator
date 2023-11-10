59ea3b9eac2da05ca369acee5c850db633cae03e
Fisch37 <seracin4@gmail.com>
2023-11-10 09:42:38 +0100

    Added get_sessionmaker function

--- data/sql/engine.py
+++ data/sql/engine.py
@@ -23,6 +23,7 @@ class AsyncDatabase(Singleton):
             self.engine,
             expire_on_commit=False
         )
+        self._opened = True
 
     @property
     def engine(self) -> asql.AsyncEngine:
@@ -39,12 +40,30 @@ class AsyncDatabase(Singleton):
         """
         return self._sessionmaker
 
+    @property
+    def is_opened(self) -> bool:
+        """
+        Returns whether or not the engine is currently open or not.
+        """
+        return self._opened
+
     async def __aenter__(self) -> Self:
         async with self.engine.begin() as connection:
             await connection.run_sync(Base.metadata.create_all)
         LOGGER.debug("Initialized database")
-
+        self._opened = True
         return self
 
     async def __aexit__(self, exc_type, exc_value, traceback) -> None:
+        self._opened = False
         await self.engine.dispose()
+
+def get_sessionmaker() -> asql.async_sessionmaker:
+    """
+    Shorthand for getting a sessionmaker from the database manager.
+    Issues a warning when getting the sessionmaker from an unopened engine.
+    """
+    database = AsyncDatabase()
+    if not database.is_opened:
+        LOGGER.warning("Got sessionmaker of unopened/closed engine!")
+    return database.sessionmaker
