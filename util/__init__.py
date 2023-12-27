a140e24a5758c667bc41909f420871f02da0e712
Fisch37 <seracin4@gmail.com>
2023-12-27 17:15:54 +0100

    Added ConfirmationView util

--- util/__init__.py
+++ util/__init__.py
@@ -5,3 +5,4 @@ from .limited_list import LimitedList
 from .webhook_pool import WebhookPool
 from .singleton import Singleton, SingletonMeta
 from .auto_stop_modal import AutoStopModal
+from .confirmation_view import ConfirmationView
